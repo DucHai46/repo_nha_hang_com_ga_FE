08ea6ce8bf55c229fa41ab945115ffd1fa3e3797
DucHai46 <b.duc.hai2003@gmail.com>
2025-03-17 23:22:59 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'repo_nha_hang_com_ga';
+}
