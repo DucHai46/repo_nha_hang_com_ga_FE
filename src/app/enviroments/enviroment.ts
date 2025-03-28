74375090357d08ddf4fe1be5137b1f85f5374972
DucHai46 <b.duc.hai2003@gmail.com>
2025-03-28 23:58:59 +0700

    sửa enviroment

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-//   production: false,
-  apiUrl: 'http://52.64.190.146:8080',
+  //   production: false,
+  // apiUrl: 'http://52.64.190.146:8080',
+  apiUrl: 'https://52.64.190.146',
 };
 
