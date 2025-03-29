9eb5d4665c39efdedb7201c93969be000f4fe0c9
beskys2love123 <nhok663@gmail.com>
2025-03-29 15:49:03 +0700

    thêm hình ảnh

--- src/app/modules/layout/menugoimon/menugoimon.component.ts
+++ src/app/modules/layout/menugoimon/menugoimon.component.ts
@@ -30,32 +30,32 @@ export class MenugoimonComponent implements OnInit {
 
   itemsMonAn = [
     // Đồ uống
-    { ma: 'MON-001', ten: 'Trà đá', hinhAnh: 'https://via.placeholder.com/150', gia: 12000, soLuong: 0, danhMuc: 'DM-002' },
-    { ma: 'MON-002', ten: 'Bình trà đá (Lớn)', hinhAnh: 'https://via.placeholder.com/150', gia: 35000, soLuong: 0, danhMuc: 'DM-002' },
-    { ma: 'MON-003', ten: 'Coca', hinhAnh: 'https://via.placeholder.com/150', gia: 15000, soLuong: 0, danhMuc: 'DM-002' },
+    { ma: 'MON-001', ten: 'Trà đá', hinhAnh: 'https://product.hstatic.net/200000385717/product/trada_c8f7b50295c5422684059c5d62342e96_1024x1024.jpg', gia: 12000, soLuong: 0, danhMuc: 'DM-002' },
+    { ma: 'MON-002', ten: 'Bình trà đá (Lớn)', hinhAnh: 'https://product.hstatic.net/200000385717/product/binhtralon_29662284998a4b4e837c13323ec3f837_1024x1024.jpg', gia: 35000, soLuong: 0, danhMuc: 'DM-002' },
+    { ma: 'MON-003', ten: 'Coca', hinhAnh: 'https://product.hstatic.net/200000385717/product/coca_942b88be6a294dff81b890487982cca5_grande.jpg', gia: 15000, soLuong: 0, danhMuc: 'DM-002' },
     { ma: 'MON-004', ten: 'Nước cam ép', hinhAnh: 'https://via.placeholder.com/150', gia: 25000, soLuong: 0, danhMuc: 'DM-004' },
     { ma: 'MON-005', ten: 'Sinh tố bơ', hinhAnh: 'https://via.placeholder.com/150', gia: 35000, soLuong: 0, danhMuc: 'DM-004' },
     { ma: 'MON-006', ten: 'Bia Tiger', hinhAnh: 'https://via.placeholder.com/150', gia: 25000, soLuong: 0, danhMuc: 'DM-013' },
     
     // Món khai vị
-    { ma: 'MON-007', ten: 'Khoai tây chiên (S)', hinhAnh: 'https://via.placeholder.com/150', gia: 35000, soLuong: 0, danhMuc: 'DM-005' },
-    { ma: 'MON-008', ten: 'Khoai tây chiên (L)', hinhAnh: 'https://via.placeholder.com/150', gia: 59000, soLuong: 0, danhMuc: 'DM-005' },
+    { ma: 'MON-007', ten: 'Khoai tây chiên (S)', hinhAnh: 'https://img.pikbest.com/png-images/qiantu/hand-drawn-cute-french-fries-cartoon-character-vector-illustration_2528693.png!sw800', gia: 35000, soLuong: 0, danhMuc: 'DM-005' },
+    { ma: 'MON-008', ten: 'Khoai tây chiên (L)', hinhAnh: 'https://img.pikbest.com/png-images/qiantu/hand-drawn-cute-french-fries-cartoon-character-vector-illustration_2528693.png!sw800', gia: 59000, soLuong: 0, danhMuc: 'DM-005' },
     { ma: 'MON-009', ten: 'Nem rán', hinhAnh: 'https://via.placeholder.com/150', gia: 25000, soLuong: 0, danhMuc: 'DM-005' },
     { ma: 'MON-010', ten: 'Chả giò', hinhAnh: 'https://via.placeholder.com/150', gia: 30000, soLuong: 0, danhMuc: 'DM-005' },
     
     // Các món chính
-    { ma: 'MON-011', ten: 'Cơm gà Hải Nam', hinhAnh: 'https://via.placeholder.com/150', gia: 65000, soLuong: 0, danhMuc: 'DM-008' },
+    { ma: 'MON-011', ten: 'Cơm gà Hải Nam', hinhAnh: 'https://product.hstatic.net/200000385717/product/com_ga_hai_nam_4dd0c831e07c40da8d30612a2b321b86_grande.jpg', gia: 65000, soLuong: 0, danhMuc: 'DM-008' },
     { ma: 'MON-012', ten: 'Cơm gà Singapore', hinhAnh: 'https://via.placeholder.com/150', gia: 70000, soLuong: 0, danhMuc: 'DM-006' },
     { ma: 'MON-013', ten: 'Cơm chảo hải sản', hinhAnh: 'https://via.placeholder.com/150', gia: 85000, soLuong: 0, danhMuc: 'DM-009' },
     { ma: 'MON-014', ten: 'Xôi gà', hinhAnh: 'https://via.placeholder.com/150', gia: 45000, soLuong: 0, danhMuc: 'DM-010' },
     
     // Combo
-    { ma: 'MON-015', ten: 'Combo 1 người', hinhAnh: 'https://via.placeholder.com/150', gia: 90000, soLuong: 0, danhMuc: 'DM-003' },
-    { ma: 'MON-016', ten: 'Combo 2 người', hinhAnh: 'https://via.placeholder.com/150', gia: 160000, soLuong: 0, danhMuc: 'DM-003' },
+    { ma: 'MON-015', ten: 'Combo 1 người', hinhAnh: 'https://product.hstatic.net/200000385717/product/combo_1_2b666b1fba224b17b53898c57ce44565_grande.jpg', gia: 90000, soLuong: 0, danhMuc: 'DM-003' },
+    { ma: 'MON-016', ten: 'Combo 2 người', hinhAnh: 'https://product.hstatic.net/200000385717/product/combo_2_5bc5ebce1f9e40d0848c7bf10602a499_grande.jpg', gia: 160000, soLuong: 0, danhMuc: 'DM-003' },
     
     // Món mới
-    { ma: 'MON-017', ten: 'Lẩu Thái', hinhAnh: 'https://img.pikbest.com/png-images/qiantu/hand-drawn-cute-french-fries-cartoon-character-vector-illustration_2528693.png!sw800', gia: 150000, soLuong: 0, danhMuc: 'DM-001' },
-    { ma: 'MON-018', ten: 'Gà nướng', hinhAnh: 'https://via.placeholder.com/150', gia: 120000, soLuong: 0, danhMuc: 'DM-001' },
+    { ma: 'MON-017', ten: 'Gà không lối thoát', hinhAnh: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg3NFRX7bet2NWdC0ZTO6TDzqzrz7SirkcyA&s', gia: 150000, soLuong: 0, danhMuc: 'DM-001' },
+    { ma: 'MON-018', ten: 'Gà nướng', hinhAnh: 'https://daynauan.info.vn/wp-content/uploads/2020/12/ga-nuong-mat-ong.jpg', gia: 120000, soLuong: 0, danhMuc: 'DM-001' },
     
     // Rau
     { ma: 'MON-019', ten: 'Rau muống xào', hinhAnh: 'https://via.placeholder.com/150', gia: 30000, soLuong: 0, danhMuc: 'DM-012' },
