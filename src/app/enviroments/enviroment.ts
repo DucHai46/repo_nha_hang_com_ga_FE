1a1fad4a0c6ae8d21dfc1e8f3da71c99bca4fcb1
DucHai46 <b.duc.hai2003@gmail.com>
2025-04-22 00:22:06 +0700

    sửa env

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   //   production: false,
   // apiUrl: 'http://52.64.190.146:8080',
-  apiUrl: 'http://52.64.190.146:8080',
-  authUrl: 'http://52.64.190.146:8081',
-  fileUrl: 'http://52.64.190.146:8082',
+  apiUrl: 'http://3.24.10.252:8080',
+  authUrl: 'http://3.24.10.252:8081',
+  fileUrl: 'http://3.24.10.252:8082',
  
 };
