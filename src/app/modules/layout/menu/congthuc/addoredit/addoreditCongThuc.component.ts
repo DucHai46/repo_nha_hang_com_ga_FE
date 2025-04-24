80fd00a7f5bea5d81eb878565d8fe6f53e567e86
beskys2love123 <nhok663@gmail.com>
2025-04-24 21:12:51 +0700

    Cong thuc

--- src/app/modules/layout/menu/congthuc/addoredit/addoreditCongThuc.component.ts
+++ src/app/modules/layout/menu/congthuc/addoredit/addoreditCongThuc.component.ts
@@ -7,63 +7,32 @@ import { LoainguyenlieuService } from '../../loainguyenlieu/services/loainguyenl
 @Component({
   selector: 'app-addoreditCongThuc',
   templateUrl: './addoreditCongThuc.component.html',
-  styleUrl: './addoreditCongThuc.component.scss'
+  styleUrls: ['./addoreditCongThuc.component.scss']
 })
 export class AddoreditCongThucComponent implements OnInit {
   nguyenLieu: any[] = [];
   loaiNguyenLieu: any[] = [];
-  filteredNguyenLieu: any[] = []; 
-  selectedLoaiNguyenLieuId: string = '';
-  selectedNguyenLieuId: string = '';
 
-  ngOnInit(): void {
+  loaiSelections: any[] = [
+    {
+      selectedLoaiId: '',
+      selectedNguyenLieuId: '',
+      filteredNguyenLieu: [],
+      nguyenLieus: []
+    }
+  ];
 
-    this.loaiNguyenLieuService.getLoaiNguyenLieu({}).subscribe({
-      next: (res: any) => {
-        this.loaiNguyenLieu = res.data.data.map((item: any) => ({
-          id: item.id,
-          name: item.tenLoai
-        }));
-
-        // if (this.isEditMode) {
-        //   const selected = this.loaiNguyenLieu.find(x => x.id === this.formData.loaiNguyenLieu.id);
-        //   if (selected) {
-        //     this.formData.loaiNguyenLieu = selected;
-        //   }
-        // }
-      },
-      error: (err: any) => console.log(err)
-    });
-    this.nguyenLieuService.getNguyenLieu({}).subscribe({
-      next: (res: any) => {
-        this.nguyenLieu = res.data.data.map((item: any) => ({
-          id: item.id,
-          name: item.tenNguyenLieu,
-          loaiNguyenLieuId: item.loaiNguyenLieu.id
-        }));
-
-      },
-      error: (err: any) => console.log(err)
-    });
-
-  }
   formData: any = {
     tenCongThuc: '',
-    nguyenLieus: [
-      {
-        nguyenLieu: {
-          id: '',
-          name: ''
-        },
-        soLuong: 0,
-        ghiChu: ''
-      }
-    ],
+    nguyenLieus: [],
     moTa: '',
     hinhAnh: ''
   };
 
-  isEditMode: boolean = false; 
+  isEditMode: boolean = false;
+
+  selectedFile: File | null = null;
+  imagePreview: string | ArrayBuffer | null = null;
 
   constructor(
     private nguyenLieuService: NguyenlieuService,
@@ -74,77 +43,114 @@ export class AddoreditCongThucComponent implements OnInit {
   ) {
     if (data && data.item) {
       this.isEditMode = true;
-      this.formData={...data.item};
+      this.formData = { ...data.item };
     }
   }
 
-  onLoaiNguyenLieuChange(): void {
-    this.filteredNguyenLieu = this.nguyenLieu.filter(
-      (nl) => nl.loaiNguyenLieuId === this.selectedLoaiNguyenLieuId
-    );
-    this.selectedNguyenLieuId = ''; // reset lựa chọn nguyên liệu
+  ngOnInit(): void {
+    this.loaiNguyenLieuService.getLoaiNguyenLieu({}).subscribe({
+      next: (res: any) => {
+        this.loaiNguyenLieu = res.data.data.map((item: any) => ({
+          id: item.id,
+          name: item.tenLoai
+        }));
+      },
+      error: (err: any) => console.log(err)
+    });
+
+    this.nguyenLieuService.getNguyenLieu({}).subscribe({
+      next: (res: any) => {
+        this.nguyenLieu = res.data.data.map((item: any) => ({
+          id: item.id,
+          name: item.tenNguyenLieu,
+          loaiNguyenLieuId: item.loaiNguyenLieu.id
+        }));
+      },
+      error: (err: any) => console.log(err)
+    });
   }
-  
-  // Khi chọn nguyên liệu -> thêm vào formData nếu chưa có
-  onNguyenLieuSelected(): void {
-    const nguyenLieu = this.nguyenLieu.find(nl => nl.id === this.selectedNguyenLieuId);
-  
-    const isAlreadySelected = this.formData.nguyenLieus.some((x:any) => x.nguyenLieu.id === nguyenLieu?.id);
-    if (nguyenLieu && !isAlreadySelected) {
-      this.formData.nguyenLieus.push({
-        nguyenLieu: {
-          id: nguyenLieu.id,
-          name: nguyenLieu.name
-        },
+
+  addLoaiSelection(): void {
+    this.loaiSelections.push({
+      selectedLoaiId: '',
+      filteredNguyenLieu: [],
+      selectedNguyenLieuId: '',
+      nguyenLieus: []
+    });
+  }
+
+  addNguyenLieuRow(index: number): void {
+    const loai = this.loaiSelections[index];
+    const availableNguyenLieus = loai.filteredNguyenLieu.filter(
+      (nl: any) => !loai.nguyenLieus.some((x: any) => x.nguyenLieu.id === nl.id)
+    );
+    if (availableNguyenLieus.length > 0) {
+      loai.nguyenLieus.push({
+        nguyenLieu: availableNguyenLieus[0],
         soLuong: 0,
         ghiChu: ''
       });
     }
   }
-  
-  removeNguyenLieu(index: number) {
-    this.formData.nguyenLieus.splice(index, 1);
+
+  onLoaiNguyenLieuChange(index: number): void {
+    const selectedLoaiId = this.loaiSelections[index].selectedLoaiId;
+    this.loaiSelections[index].filteredNguyenLieu = this.nguyenLieu.filter(
+      (nl) => nl.loaiNguyenLieuId === selectedLoaiId
+    );
+    this.loaiSelections[index].selectedNguyenLieuId = '';
+    this.loaiSelections[index].nguyenLieus = [];
   }
+
+  isLoaiDuplicate(selectedLoaiId: string, index: number): boolean {
+    return this.loaiSelections.some((s, idx) => idx !== index && s.selectedLoaiId === selectedLoaiId);
+  }
+
+  removeNguyenLieuInLoai(loaiIndex: number, nlIndex: number): void {
+    this.loaiSelections[loaiIndex].nguyenLieus.splice(nlIndex, 1);
+  }
+
+  removeLoaiSelection(index: number): void {
+    this.loaiSelections.splice(index, 1);
+  }
+
   onSave(): void {
-    const validNguyenLieus = this.formData.nguyenLieus
-      .filter((x: any) => x.nguyenLieu?.id)      // chỉ giữ những mục đã chọn nguyên liệu
-      .map((x: any) => ({                        // rồi map ra đúng format API
+    const allNguyenLieus = this.loaiSelections.flatMap((loai: any) =>
+      loai.nguyenLieus.map((item: any) => ({
         nguyenLieu: {
-          id: x.nguyenLieu.id,
-          name: x.nguyenLieu.name
+          id: item.nguyenLieu.id,
+          name: item.nguyenLieu.name
         },
-        soLuong: x.soLuong,
-        ghiChu: x.ghiChu
-      }));
-  
+        soLuong: item.soLuong,
+        ghiChu: item.ghiChu
+      }))
+    );
+
     const dataToSend = {
       tenCongThuc: this.formData.tenCongThuc,
       moTa: this.formData.moTa,
       hinhAnh: this.formData.hinhAnh.name,
-      nguyenLieus: validNguyenLieus
+      nguyenLieus: allNguyenLieus
     };
-  
+
     this.dialogRef.close(dataToSend);
   }
-  
+
   onCancel(): void {
-    this.dialogRef.close(); 
+    this.dialogRef.close();
   }
-  selectedFile: File | null = null;
-  imagePreview: string | ArrayBuffer | null = null;
-  onFileSelected(event: any) {
+
+  onFileSelected(event: any): void {
     const file: File = event.target.files[0];
     if (file) {
       this.selectedFile = file;
 
-      // Tạo preview ảnh
       const reader = new FileReader();
       reader.onload = () => {
         this.imagePreview = reader.result;
       };
       reader.readAsDataURL(file);
 
-      // Upload file ngay sau khi chọn
       const formData = new FormData();
       formData.append('file', file);
 
@@ -157,7 +163,6 @@ export class AddoreditCongThucComponent implements OnInit {
               id: fileId,
               name: fileName
             };
-            console.log('Uploaded:', this.formData.hinhAnh);
           } else {
             console.error('Upload failed', res);
           }
@@ -168,5 +173,4 @@ export class AddoreditCongThucComponent implements OnInit {
       });
     }
   }
-  
 }
--- src/app/modules/layout/menu/nguyenlieu/nguyenlieu.component.ts
+++ src/app/modules/layout/menu/nguyenlieu/nguyenlieu.component.ts
@@ -148,32 +148,83 @@ export class NguyenlieuComponent implements OnInit {
       }
     });
   }
-      openEditPopup(item: any): void {
-        const dialogRef = this.dialog.open(AddoreditNguyenLieuComponent, {
-          width: '400px',
-          data: { item }, // Truyền dữ liệu của nguyên liệu cần sửa
-        });
-    
-        dialogRef.afterClosed().subscribe((result) => {
-          if (result) {
-          this.nguyenlieuService.updateNguyenLieu(item.id, result).subscribe(
-            {
-              next: (res: any) => {
-                if(res.data){
-                  this.search();
-                }
-                else{
-                  alert('Sửa thất bại');
-                }
-              },
-              error: (err: any) => {
-                alert('Sửa thất bại');
-              }
+  openEditPopup(item: any): void {
+    const dialogRef = this.dialog.open(AddoreditNguyenLieuComponent, {
+      width: '400px',
+      data: { item }, // Truyền dữ liệu của nguyên liệu cần sửa
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+      this.nguyenlieuService.updateNguyenLieu(item.id, result).subscribe(
+        {
+          next: (res: any) => {
+            if(res.data){
+              this.search();
             }
-          )
+            else{
+              alert('Sửa thất bại');
+            }
+          },
+          error: (err: any) => {
+            alert('Sửa thất bại');
           }
-        });
+        }
+      )
       }
+    });
+  }
+
+  openAddPopup(): void {
+    const dialogRef = this.dialog.open(AddoreditNguyenLieuComponent, {
+      width: '400px',
+      data: {}, // Không truyền dữ liệu vì là Thêm
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.nguyenlieuService.addNguyenLieu(result).subscribe(
+        {
+          next: (res: any) => {
+            if (res.data) {
+              // alert('Thêm mới thành công');
+              this.searchForm.tenNguyenLieu = '';
+              this.searchForm.loaiNguyenLieuId = '';
+              this.searchForm.tuDoId = '';
+              this.searchForm.donViTinhId = '';
+              this.searchForm.trangThai = '';
+              this.search();
+            }
+            else{
+              alert('Thêm mới thất bại');
+            }
+          },
+          error: (err: any) => {
+            alert('Thêm mới thất bại');
+          }
+        }
+      )
+        // this.notification.success(
+        //   'Thành công', // Tiêu đề
+        //   'Thêm dữ liệu thành công', // Nội dung
+        //   {
+        //     nzDuration: 3000, // Thời gian hiển thị (ms)
+        //     nzPlacement: 'topRight', // Đặt vị trí là góc trên phải
+        //   }
+        // );
+      }
+      else{
+        // this.notification.error(
+        //   'Thành công', // Tiêu đề
+        //   'Thêm dữ liệu thất bại', // Nội dung
+        //   {
+        //     nzDuration: 3000, // Thời gian hiển thị (ms)
+        //     nzPlacement: 'topRight', // Đặt vị trí là góc trên phải
+        //   }
+        // );
+      }
+    });
+  }
 
 
 }
